5858ddca8eaed8b7a459802a0cce4aeb1082cb85
globalq64 <globalq@naver.com>
2020-11-17 11:18:19 +0900

    Updates

---
+++ precache-manifest.c7f9f9ad80d19f06b8f3ebc2e84d0b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce59cb7d555c341e3c0638f2728b0dd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "803e8f6bf3f2cd4f73c3",
+    "url": "/movie_app_2020/static/css/main.3a691e50.chunk.css"
+  },
+  {
+    "revision": "0089a64a68eef2bb5e64",
+    "url": "/movie_app_2020/static/js/2.f13fc3e8.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/movie_app_2020/static/js/2.f13fc3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803e8f6bf3f2cd4f73c3",
+    "url": "/movie_app_2020/static/js/main.bd418431.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
